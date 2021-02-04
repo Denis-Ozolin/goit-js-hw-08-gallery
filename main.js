11a7d85dd2dc08120d3c75f1839f66db37be9c8c
Denis-Ozolin <ozolindenx@gmail.com>
2021-02-04 02:42:50 +0200

    in progress...

--- main.js
+++ main.js
@@ -1,27 +1,56 @@
 import galleryItems from "./gallery-items.js"
-
-
-const xxx = [  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },]
+  
 const galleryRef = document.querySelector('.js-gallery');
+const lightboxRef = document.querySelector('.js-lightbox');
+const lightboxImageRef = document.querySelector('img.lightbox__image');
+const closeBtnRef = document.querySelector('button[data-action="close-lightbox"]');
+const lightboxOverlayRef = document.querySelector('div.lightbox__overlay');
 
-const previewGalleryRefs = xxx.map(el => {
-    `<li><img src = '${el.preview} alt = '${el.description}'></li>`;
-}).join('');
-
-
-console.log(previewGalleryRefs);
+const previewGalleryRefs = galleryItems.map(el => 
+  `<li class = 'gallery__item'><img src = '${el.preview}' 
+  alt = '${el.description}' class = 'gallery__image' 
+  data-link = '${el.original}'></li>`)
+  .join('');   
 
 galleryRef.insertAdjacentHTML('beforeend', previewGalleryRefs);
+
+galleryRef.addEventListener('click', (event => {
+  event.preventDefault();
+
+  lightboxRef.classList.add('is-open');
+  lightboxImageRef.src = event.target.dataset.link;
+
+
+  if (event.target.nodeName !== 'IMG') return;
+  lightboxImageRef.src = event.target.dataset.link;
+}));
+
+closeBtnRef.addEventListener('click', () => {
+  lightboxRef.classList.remove('is-open');
+  lightboxImageRef.src = '';
+})
+
+lightboxOverlayRef.addEventListener('click', () => {
+  lightboxRef.classList.remove('is-open');
+  lightboxImageRef.src = '';
+})
+
+window.addEventListener('keydown', event => {
+  if (event.code === 'Escape') {
+    lightboxRef.classList.remove('is-open');
+    lightboxImageRef.src = '';
+  };
+
+  // if (event.code === 'ArrowLeft') {
+  //   lightboxRef.src = lightboxRef.previousElementSibling
+  //     .firstElementChild.dataset.link;
+  // }
+
+  //  if (event.code === 'ArrowRight') {
+  //   lightboxRef.src = lightboxRef.nextElementSibling
+  //     .firstElementChild.dataset.link;
+  // }
+
+});
+   
+
