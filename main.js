fc5e7bf19108e2ba52cd463293dac20a4175bea8
Denis-Ozolin <ozolindenx@gmail.com>
2021-02-06 01:35:53 +0200

    hw in process

--- main.js
+++ main.js
@@ -6,51 +6,71 @@ const lightboxImageRef = document.querySelector('img.lightbox__image');
 const closeBtnRef = document.querySelector('button[data-action="close-lightbox"]');
 const lightboxOverlayRef = document.querySelector('div.lightbox__overlay');
 
+const originalImgList = galleryItems.map(el => el.original);
+
 const previewGalleryRefs = galleryItems.map(el => 
-  `<li class = 'gallery__item'><img src = '${el.preview}' 
+  `<li class = 'gallery__item'><a class = 'gallery__link' href = '${el.original}'><img src = '${el.preview}' 
   alt = '${el.description}' class = 'gallery__image' 
-  data-link = '${el.original}'></li>`)
-  .join('');   
+  data-link = '${el.original}'></a></li>`)
+  .join(''); 
 
 galleryRef.insertAdjacentHTML('beforeend', previewGalleryRefs);
+  
+function onOpenModalWindow() {
+  event.preventDefault();
+  
+  if (event.target.nodeName !== 'IMG') return;
+  lightboxRef.classList.add('is-open');
+
+  lightboxImageRef.src = event.target.dataset.link;
+}   
+
+function onCloseModalWindow() {
+  lightboxRef.classList.remove('is-open');
+  lightboxImageRef.src = '';
+}
+
+function onChangeImgToBefore() {
+  originalImgList.forEach((el, inx, arr) => {
+    if (lightboxImageRef.src === el) {
+      if (inx === 0) return;
+        lightboxImageRef.src = arr[inx - 1];
+    }
+  })
+}
+
+function onChangeImgToAfter() {
+    originalImgList.forEach((el, inx, arr) => {
+      if (lightboxImageRef.src === el) {
+        if (inx === arr.length - 1) return;
+        lightboxImageRef.src = arr[inx + 1];
+    }
+  })
+}
 
 galleryRef.addEventListener('click', (event => {
-  event.preventDefault();
-
-  lightboxRef.classList.add('is-open');
-  lightboxImageRef.src = event.target.dataset.link;
-
-
-  if (event.target.nodeName !== 'IMG') return;
-  lightboxImageRef.src = event.target.dataset.link;
+  onOpenModalWindow();
 }));
 
 closeBtnRef.addEventListener('click', () => {
-  lightboxRef.classList.remove('is-open');
-  lightboxImageRef.src = '';
+  onCloseModalWindow();
 })
 
 lightboxOverlayRef.addEventListener('click', () => {
-  lightboxRef.classList.remove('is-open');
-  lightboxImageRef.src = '';
+  onCloseModalWindow();
 })
 
 window.addEventListener('keydown', event => {
   if (event.code === 'Escape') {
-    lightboxRef.classList.remove('is-open');
-    lightboxImageRef.src = '';
+    onCloseModalWindow();
   };
 
-  // if (event.code === 'ArrowLeft') {
-  //   lightboxRef.src = lightboxRef.previousElementSibling
-  //     .firstElementChild.dataset.link;
-  // }
+  if (event.code === 'ArrowLeft') {
+    onChangeImgToBefore();
+  }
 
-  //  if (event.code === 'ArrowRight') {
-  //   lightboxRef.src = lightboxRef.nextElementSibling
-  //     .firstElementChild.dataset.link;
-  // }
+  if (event.code === 'ArrowRight') {
+    onChangeImgToAfter();
+  }
 
 });
-   
-
